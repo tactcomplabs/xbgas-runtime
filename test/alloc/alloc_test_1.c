ed269851c1c1fa380806d3e51caca837a41e375e
jleidel <jleidel@tactcomplabs.com>
2018-06-07 15:58:57 -0500

    adding tests to catch erroneous conditions in allocatings

---
+++ test/alloc/alloc_test_1.c
@@ -0,0 +1,41 @@
+/* _ALLOC_TEST_1_C_
+ *
+ * Copyright (C) 2017-2018 Tactical Computing Laboratories, LLC
+ * All Rights Reserved
+ * contact@tactcomplabs.com
+ *
+ * This file is a part of the XBGAS-RUNTIME package.  For license
+ * information, see the LICENSE file in the top level directory
+ * of the distribution.
+ *
+ */
+
+#include <stdio.h>
+#include "xbrtime.h"
+
+#define _XBGAS_ALLOC_SIZE_ 8
+
+int main( int argc, char **argv ){
+  int rtn = 0;
+  void *ptr = NULL;
+  size_t sz = _XBGAS_ALLOC_SIZE_;
+
+  printf( "Initializing xBGAS Runtime\n" );
+  rtn = xbrtime_init();
+  printf( "xBGAS is Initialized\n" );
+
+  printf( "Allocating %d bytes\n", (int)(sz) );
+  ptr = xbrtime_malloc( sz );
+
+  printf( "PTR = %p\n", ptr );
+
+  xbrtime_free( ptr );
+
+  printf( "xBGAS is Closing\n" );
+  xbrtime_close();
+  printf( "xBGAS is Closed\n" );
+
+  return rtn;
+}
+
+/* EOF */
---
+++ test/alloc/alloc_test_KF_2.c
@@ -0,0 +1,49 @@
+/* _ALLOC_TEST_KF_2_C_
+ *
+ * Copyright (C) 2017-2018 Tactical Computing Laboratories, LLC
+ * All Rights Reserved
+ * contact@tactcomplabs.com
+ *
+ * This file is a part of the XBGAS-RUNTIME package.  For license
+ * information, see the LICENSE file in the top level directory
+ * of the distribution.
+ *
+ */
+
+#include <stdio.h>
+#include "xbrtime.h"
+
+#define _XBGAS_ALLOC_SIZE_ 100000000000000000
+
+int main( int argc, char **argv ){
+  int rtn = 0;
+  void *ptr = NULL;
+  size_t sz = _XBGAS_ALLOC_SIZE_;
+
+  printf( "Initializing xBGAS Runtime\n" );
+  rtn = xbrtime_init();
+  printf( "xBGAS is Initialized\n" );
+
+  printf( "Allocating %d bytes\n", (int)(sz) );
+  ptr = xbrtime_malloc( sz );
+  if( ptr == NULL ){
+    printf( "Successfully caught erroneous condition\n" );
+    xbrtime_close();
+    return 0;
+  }else{
+    printf( "Erroneously allocated 0 bytes of memory\n" );
+    rtn = -1;
+  }
+
+  printf( "PTR = %p\n", ptr );
+
+  xbrtime_free( ptr );
+
+  printf( "xBGAS is Closing\n" );
+  xbrtime_close();
+  printf( "xBGAS is Closed\n" );
+
+  return rtn;
+}
+
+/* EOF */
